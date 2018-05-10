0b80f9e73f338da295654ac9a9d67f7c16cd682e
Ryan Carter <me@rncrtr.in>
2018-05-10 06:10:55 +0000

    fixed nav menu in place

--- app/components/nav-menu.js
+++ app/components/nav-menu.js
@@ -5,8 +5,7 @@ angular.module('navMenu', ['ngRoute'])
 .directive('navMenu', function() {
   return {
     template: 
-    
-        '<ul class="nav bg-dark col-12-sm nopad">'+
+        '<ul id="wfc-menu" class="nav bg-dark col-12-sm nopad">'+
           '<li class="logo bg-dark col-1-sm" style="padding-left: 10px;"><a href="#!/view1"><img style="padding-top: 5px;" width="30px" src="../assets/images/wfc-logo.png" /></a></li>'+
           '<li class="nav-item col-2-sm">'+
             '<a class="nav-link text-white" href="#!/view1">Home</a>'+
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -14,12 +14,31 @@ angular.module('myApp.view1', ['ngRoute'])
     $scope.menuItems = [
       {name:'Announcements',url: ''},
       {name:'Services & Contact Info',url: '#!/services'},
-      {name:'Map',url:'https://www.google.com/maps/place/4625+E+Iowa+Ave,+Denver,+CO+80222/@39.6836303,-104.9332396,15z/data=!4m7!1m4!3m3!1s0x876c7dde82eac263:0x2912172574625955!2s4625+E+Iowa+Ave,+Denver,+CO+80222!3b1!3m1!1s0x876c7dde82eac263:0x2912172574625955'},
+      {name:'Map',url:'https://www.google.com/maps/place/4625+E+Iowa+Ave,+Denver,+CO+80222/@39.6836303,-104.9332396,15z/data=!4m7!1m4!3m3!1s0x876c7dde82eac263:0x2912172574625955!2s4625+E+Iowa+Ave,+Denver,+CO+80222!3b1!3m1!1s0x876c7dde82eac263:0x2912172574625955',ext: true},
       {name:'Discipleship',url:'http://whiteflagcalvary.org/discipleship'},
       {name:'Giving',url:'#!/giving'},
-      {name:'Food Bank Info',url:'#!/foodbank'},
-      {name:'YouTube Videos',url:'https://m.youtube.com/channel/UCNum-_XTF3mmxVUzVRHki8Q'},
-      {name:'Facebook Page',url:'https://m.facebook.com/whiteflagcalvary'},
+      {name:'Love Abounds Food Bank',url:'#!/foodbank'},
+      {name:'YouTube Videos',url:'https://m.youtube.com/channel/UCNum-_XTF3mmxVUzVRHki8Q',ext: true},
+      {name:'Facebook Page',url:'https://m.facebook.com/whiteflagcalvary',ext: true},
       {name:'About The App',url:'#!/about'}
     ];
+    
+    var v = document.getElementById('welcome_video');
+    v.addEventListener(
+       'play', 
+          function() { 
+             v.play();
+          }, 
+        false);
+
+    v.onclick = function() {
+      if (v.paused) {
+        v.play();
+        v.controls=null;
+      } else {
+        v.pause();
+        v.controls="controls";
+      }
+    };
+
 }]);
