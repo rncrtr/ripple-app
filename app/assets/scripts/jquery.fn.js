a1807901c717eab9c9c80e60fdd35c80e1151abc
Ryan Carter <me@rncrtr.in>
2018-05-30 19:25:25 +0000

    fixed push switch hide on hashchange

--- app/assets/scripts/jquery.fn.js
+++ app/assets/scripts/jquery.fn.js
@@ -1,4 +1,13 @@
+/* global $ */
 $(document).ready(function () {
-
+   
     
+});
+
+ $(window).on('hashchange',function(){
+    if(window.location.href.indexOf('view1')!=-1){
+        $('.pushupd-switch').show();
+    }else{
+        $('.pushupd-switch').hide();
+    }
 });
