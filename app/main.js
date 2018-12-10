fca53b2dacbdc8954dbf343cdaff2b6ccf946c82
Ryan Carter <me@rncrtr.in>
2018-12-09 23:08:54 -0700

    remove more PN code

--- app/main.js
+++ app/main.js
@@ -20,123 +20,123 @@ if ('serviceWorker' in navigator) {
 window.addEventListener('beforeinstallprompt', function(event) {
   console.log('beforeinstallprompt fired');
   //event.preventDefault();
-  deferredPrompt = event;
+  //deferredPrompt = event;
   // for (var i = 0; i < installAppButtons.length; i++) {
   //   installAppButtons[i].style.display = 'block';
   // }
 });
 
-function displayConfirmNotification() {
-  if ('serviceWorker' in navigator) {
-    var options = {
-      body: 'You successfully subscribed to our Notification service!',
-      icon: '/src/images/icons/app-icon-96x96.png',
-      image: '/src/images/sf-boat.jpg',
-      dir: 'ltr',
-      lang: 'en-US', // BCP 47,
-      vibrate: [100, 50, 200],
-      badge: '/src/images/icons/app-icon-96x96.png',
-      tag: 'confirm-notification',
-      renotify: true,
-      actions: [
-        { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
-        { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
-      ]
-    };
+// function displayConfirmNotification() {
+//   if ('serviceWorker' in navigator) {
+//     var options = {
+//       body: 'You successfully subscribed to our Notification service!',
+//       icon: '/src/images/icons/app-icon-96x96.png',
+//       image: '/src/images/sf-boat.jpg',
+//       dir: 'ltr',
+//       lang: 'en-US', // BCP 47,
+//       vibrate: [100, 50, 200],
+//       badge: '/src/images/icons/app-icon-96x96.png',
+//       tag: 'confirm-notification',
+//       renotify: true,
+//       actions: [
+//         { action: 'confirm', title: 'Okay', icon: '/src/images/icons/app-icon-96x96.png' },
+//         { action: 'cancel', title: 'Cancel', icon: '/src/images/icons/app-icon-96x96.png' }
+//       ]
+//     };
 
-    navigator.serviceWorker.ready
-      .then(function(swreg) {
-        swreg.showNotification('Successfully subscribed!', options);
-        configurePushSub();
-      });
-  }
-}
+//     navigator.serviceWorker.ready
+//       .then(function(swreg) {
+//         swreg.showNotification('Successfully subscribed!', options);
+//         configurePushSub();
+//       });
+//   }
+// }
 
-function configurePushSub() {
-  if (!('serviceWorker' in navigator)) {
-    return;
-  }
+// function configurePushSub() {
+//   if (!('serviceWorker' in navigator)) {
+//     return;
+//   }
 
-  var reg;
-  navigator.serviceWorker.ready
-    .then(function(swreg) {
-      reg = swreg;
-      return swreg.pushManager.getSubscription();
-    })
-    .then(function(sub) {
-        console.log('sub: ',sub);
-      if (sub === null) {
-        // Create a new subscription
-        var vapidPublicKey = 'BC1qaKiOtzHYJ_1ZC0pODkVDMuwa-8VHkxNawBvVKBRDkrauLrb0B9Lj5u9TiBm9HjJYo-io_MWSSSn2fii3YW4';
-        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
-        return reg.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: convertedVapidPublicKey
-        });
+//   var reg;
+//   navigator.serviceWorker.ready
+//     .then(function(swreg) {
+//       reg = swreg;
+//       return swreg.pushManager.getSubscription();
+//     })
+//     .then(function(sub) {
+//         console.log('sub: ',sub);
+//       if (sub === null) {
+//         // Create a new subscription
+//         var vapidPublicKey = 'BC1qaKiOtzHYJ_1ZC0pODkVDMuwa-8VHkxNawBvVKBRDkrauLrb0B9Lj5u9TiBm9HjJYo-io_MWSSSn2fii3YW4';
+//         var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+//         return reg.pushManager.subscribe({
+//           userVisibleOnly: true,
+//           applicationServerKey: convertedVapidPublicKey
+//         });
 
-      } else {
-        // We have a subscription
-        console.log('existing sub');
-        for (var i = 0; i < enableNotificationsButtons.length; i++) {
-          //enableNotificationsButtons[i].innerHTML = 'Already enabled!';
-          enableNotificationsButtons[i].checked = true;
-          enableNotificationsButtons[i].disabled = true;
-        }
-        return 'exists';
-      }
-    })
-    .then(function(newSub) {
-        if(newSub!=='exists'){
-            return fetch('https://whiteflagmobile.com/api/push/sub', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-                },
-                body: JSON.stringify(newSub)
-            })
-        }else{
-            return 'exists';
-        }
-    })
-    .then(function(res) {
-      if (res!=='exists' && res.ok) {
-        displayConfirmNotification();
-      }
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
-}
+//       } else {
+//         // We have a subscription
+//         console.log('existing sub');
+//         for (var i = 0; i < enableNotificationsButtons.length; i++) {
+//           //enableNotificationsButtons[i].innerHTML = 'Already enabled!';
+//           enableNotificationsButtons[i].checked = true;
+//           enableNotificationsButtons[i].disabled = true;
+//         }
+//         return 'exists';
+//       }
+//     })
+//     .then(function(newSub) {
+//         if(newSub!=='exists'){
+//             return fetch('https://whiteflagmobile.com/api/push/sub', {
+//                 method: 'POST',
+//                 headers: {
+//                   'Content-Type': 'application/json',
+//                   'Accept': 'application/json'
+//                 },
+//                 body: JSON.stringify(newSub)
+//             })
+//         }else{
+//             return 'exists';
+//         }
+//     })
+//     .then(function(res) {
+//       if (res!=='exists' && res.ok) {
+//         displayConfirmNotification();
+//       }
+//     })
+//     .catch(function(err) {
+//       console.log(err);
+//     });
+// }
 
-function askForNotificationPermission() {
-  Notification.requestPermission(function(result) {
-    console.log('User Choice', result);
-    if (result !== 'granted') {
-      console.log('No notification permission granted!');
-    } else {
-      configurePushSub();
-      //ga('send', { hitType: 'event', eventCategory: 'PushNotifications', eventAction: 'enable', eventLabel: 'PushSubs' });
-      // displayConfirmNotification();
-    }
-  });
-}
+// function askForNotificationPermission() {
+//   Notification.requestPermission(function(result) {
+//     console.log('User Choice', result);
+//     if (result !== 'granted') {
+//       console.log('No notification permission granted!');
+//     } else {
+//       configurePushSub();
+//       //ga('send', { hitType: 'event', eventCategory: 'PushNotifications', eventAction: 'enable', eventLabel: 'PushSubs' });
+//       // displayConfirmNotification();
+//     }
+//   });
+// }
 
-function installAppToHomescreen(){
-  console.log('install button clicked');
-  deferredPrompt.prompt();
-  deferredPrompt.userChoice
-  .then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-        //ga('send', { hitType: 'event', eventCategory: 'AppInstalls', eventAction: 'install', eventLabel: 'WhiteFlagPWA' });
-      } else {
-        console.log('User dismissed the A2HS prompt');
-        //ga('send', { hitType: 'event', eventCategory: 'AppInstalls', eventAction: 'decline', eventLabel: 'WhiteFlagPWA' });
-      }
-      deferredPrompt = null;
-    });
-}
+// function installAppToHomescreen(){
+//   console.log('install button clicked');
+//   deferredPrompt.prompt();
+//   deferredPrompt.userChoice
+//   .then((choiceResult) => {
+//     if (choiceResult.outcome === 'accepted') {
+//         console.log('User accepted the A2HS prompt');
+//         //ga('send', { hitType: 'event', eventCategory: 'AppInstalls', eventAction: 'install', eventLabel: 'WhiteFlagPWA' });
+//       } else {
+//         console.log('User dismissed the A2HS prompt');
+//         //ga('send', { hitType: 'event', eventCategory: 'AppInstalls', eventAction: 'decline', eventLabel: 'WhiteFlagPWA' });
+//       }
+//       deferredPrompt = null;
+//     });
+// }
 
 // window.addEventListener('appinstalled', (evt) => {
 //   for (var i = 0; i < installAppButtons.length; i++) {
@@ -165,16 +165,16 @@ function urlBase64ToUint8Array(base64String) {
   return outputArray;
 }
 
-if ('Notification' in window && 'serviceWorker' in navigator) {
-  for (var i = 0; i < enableNotificationsButtons.length; i++) {
-    enableNotificationsButtons[i].style.display = 'inline-block';
-    enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
-  }
-}
+// if ('Notification' in window && 'serviceWorker' in navigator) {
+//   for (var i = 0; i < enableNotificationsButtons.length; i++) {
+//     enableNotificationsButtons[i].style.display = 'inline-block';
+//     enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
+//   }
+// }
 
-if(deferredPrompt){
-  for (var i = 0; i < installAppButtons.length; i++) {
-    installAppButtons[i].addEventListener('click',installAppToHomescreen);
-  }
-}
-configurePushSub();
+// if(deferredPrompt){
+//   for (var i = 0; i < installAppButtons.length; i++) {
+//     installAppButtons[i].addEventListener('click',installAppToHomescreen);
+//   }
+// }
+// configurePushSub();
